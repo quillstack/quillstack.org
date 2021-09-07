b8b17492bf486031d64f564c469761b53f030884
Radoslaw Ziemniewicz <admin@verbanent.com>
2021-09-07 21:27:57 +0100

    Changed logo font

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,8 @@
 <template>
     <div class="p-10 mx-auto xl:w-13 lg:w-1/2 md:w-2/3">
-        <a href="/">
-            <img src="@/static/favicon.svg" class="w-[100px]">
+        <a href="/" class="flex">
+            <img src="@/static/favicon.svg" class="w-[40px] block" width="40px">
+            <div class="logo pt-1 pl-2">Quillstack</div>
         </a>
     </div>
 </template>
