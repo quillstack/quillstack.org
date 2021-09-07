3cd0e0b9a90b780ff91343cb90e3db6e63b46c13
Radoslaw Ziemniewicz <admin@verbanent.com>
2021-09-07 21:01:40 +0100

    Added JIT mode to Tailwind

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="p-10 mx-auto xl:w-13 lg:w-1/2 md:w-2/3">
         <a href="/">
-            <img src="@/static/favicon.svg" class="w-1/12">
+            <img src="@/static/favicon.svg" class="w-[100px]">
         </a>
     </div>
 </template>
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    mode: 'jit',
+    purge: [
+        './docs/*.html',
+        './pages/*.{js,jsx,ts,tsx,vue}',
+        './components/*.{js,jsx,ts,tsx,vue}',
+    ],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+}
