a4faadea950863635a61edbce0c23da0cf95ba89
Radoslaw Ziemniewicz <admin@verbanent.com>
2021-09-14 20:41:45 +0100

    Updated link in header (nuxt-link)

--- components/Logo.vue
+++ components/Logo.vue
@@ -1,10 +1,10 @@
 <template>
-    <a href="/" class="logo">
+    <nuxt-link to="/" class="logo">
         <div class="center flex">
             <img src="@/static/favicon.svg" class="w-[30px] block" width="30px" height="30px">
             <div class="quillstack">Quillstack</div>
         </div>
-    </a>
+    </nuxt-link>
 </template>
 
 <script>
