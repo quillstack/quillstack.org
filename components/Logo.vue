4435476c9f2f7fa861e1df2be759caafb212ef38
Radoslaw Ziemniewicz <admin@verbanent.com>
2021-09-15 19:15:47 +0100

    Added vue-headroom to the project (header)

--- components/Logo.vue
+++ components/Logo.vue
@@ -1,14 +1,20 @@
 <template>
-    <nuxt-link to="/" class="logo">
-        <div class="center flex">
-            <img src="@/static/favicon.svg" class="w-[30px] block" width="30px" height="30px">
-            <div class="quillstack">Quillstack</div>
-        </div>
-    </nuxt-link>
+    <headroom>
+        <nuxt-link to="/" class="logo">
+            <div class="center flex">
+                <img src="@/static/favicon.svg" class="w-[30px] block" width="30px" height="30px">
+                <div class="quillstack">Quillstack</div>
+            </div>
+        </nuxt-link>
+    </headroom>
 </template>
 
 <script>
+import { headroom } from 'vue-headroom';
 export default {
-    name: "Logo"
+    name: "Logo",
+    components: {
+        headroom
+    }
 }
 </script>
